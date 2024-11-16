fdde79f4118a977a3580bb0087979eb8c41403a9
gega109 <bakuravar1@gmail.com>
2024-11-17 00:13:56 +0400

    homework26

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
+let number = 1;
+let btn = document.getElementById("btn");
+
 async function getProducts() {
-  let number = 1;
-  let btn = document.getElementById("btn");
-  btn.addEventListener("click", async () => {
+  try {
     let data = await fetch(`https://fakestoreapi.com/products/${number}`);
-    try {
-      let products = await data.json();
-      let p = document.createElement("p");
-      p.textContent = `სახელი: ${products.title} --- ფასი: ${products.price}`;
-      document.body.appendChild(p);
-      number++;
-    } catch (error) {
-      let h1 = document.createElement("h1");
-      h1.textContent = "ხარვეზია";
-      document.body.appendChild(h1);
-      h1.style.textAlign = "center";
-      h1.style.color = "red";
-      h1.style.fontSize = "60px";
-    }
-  });
+    let products = await data.json();
+    let p = document.createElement("p");
+    p.textContent = `სახელი: ${products.title} --- ფასი: ${products.price}`;
+    document.body.appendChild(p);
+  } catch (error) {
+    let h1 = document.createElement("h1");
+    h1.textContent = "ხარვეზია";
+    document.body.appendChild(h1);
+    h1.style.textAlign = "center";
+    h1.style.color = "red";
+    h1.style.fontSize = "60px";
+  }
 }
-getProducts();
+
+btn.addEventListener("click", () => {
+  getProducts();
+  number++;
+});
